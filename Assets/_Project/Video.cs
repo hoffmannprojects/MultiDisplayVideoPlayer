ab7cc08e5eb77b09ea20fb42976d97cce1c01cf0
Tim <9947384+hoffmannprojects@users.noreply.github.com>
2018-06-09 14:49:01 +0200

    Added base components for video playback.

---
+++ Assets/_Project/Video.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class Video : MonoBehaviour
+{
+    private VideoPlayer videoPlayer;
+
+    // Use this for initialization
+    void Start()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+
+        videoPlayer.url = "Assets/file.mp4";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
