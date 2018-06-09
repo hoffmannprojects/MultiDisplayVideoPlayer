cb91693f840a42320c76ee3dde02940283efe19c
Tim <9947384+hoffmannprojects@users.noreply.github.com>
2018-06-09 16:00:29 +0200

    Added start/stop button and networkmanager.

---
+++ Assets/_Project/VideoManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.Video;
+
+public class VideoManager : MonoBehaviour
+{
+    private const string videoUrl = "D:\\Tim\\Documents\\Github\\MultiDisplayVideoPlayer\\file.mov";
+    private VideoPlayer videoPlayer;
+
+    // Use this for initialization
+    void Start()
+    {
+        videoPlayer = Camera.main.GetComponent<VideoPlayer>();
+        Assert.IsNotNull(videoPlayer);
+
+        videoPlayer.url = videoUrl;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void TogglePlayback ()
+    {
+        if (!videoPlayer.isPlaying)
+        {
+            videoPlayer.Play();
+        }
+        else
+        {
+            videoPlayer.Stop();
+        }
+    }
+}
